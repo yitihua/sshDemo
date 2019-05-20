199747e63b122031c3f3427f5e27e898ba93c051
BUZA2018 <44996632+BUZA2018@users.noreply.github.com>
2019-05-20 23:14:06 +0800

    Add files via upload

--- sshDemo/src/main/webapp/static/js/indexStart.js
+++ sshDemo/src/main/webapp/static/js/indexStart.js
@@ -1,3 +1,4 @@
+var ctx = pageContext.request.contextPath
 //用一个json形式的data变量储存你要传输的数据
 var data ={
     name: "buza",
@@ -11,17 +12,56 @@ function join(data){
      }
     return str.replace(/&$/, "")
 }
+//删除某项
 //获取删除按钮,并操作删除操作
-var delBtns = document.getElementsByClassName("btn-red")
+var delBtns = document.getElementsByClassName("delect")
 for(let i = 0,l = delBtns.length;i<l;i++){
     let newsid = delBtns[i].getAttribute("flag")
-    delBtns[i].onclick = function delectNews(){
+    delBtns[i].onclick = function delectNews() {
+        let xhr = new XMLHttpRequest()
+        //post方法如下
+        //删除操作的地址↓↓↓↓
+        xhr.open("post", "http://localhost:8080?", true)
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        xhr.send(newsid)
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+                    console.log("sussees")
+                }
+            }
+        }
+    }
+}
+//编辑某项
+var editBtns = document.getElementsByClassName("edit")
+for(let i = 0,l = editBtns.length;i<l;i++){
+    let newsid = editBtns[i].getAttribute("flag")
+    editBtns[i].onclick = function editNews() {
+        let xhr = new XMLHttpRequest()
+        //post方法如下
+        //删除操作的地址↓↓↓↓
+        xhr.open("post", "http://localhost:8080?", true)
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+        xhr.send(newsid)
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+                    console.log("sussees")
+                }
+            }
+        }
+    }
+}
+//新增项
+var addBtn = document.getElementById("add")
+
+addBtn.onclick = function addNews(){
     let xhr =new XMLHttpRequest()
-    //post方法如下
-                        //删除操作的地址↓↓↓↓
-       xhr.open("post", "http://localhost:8080?", true)
-       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-       xhr.send(newsid)
+    //get方法如下
+                        //增加操作的地址↓↓↓↓
+       xhr.open("get","http://localhost:8080?"+join(data),true)
+       xhr.send()
        xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
                 if(xhr.status>=200 && xhr.status<300 || xhr.status===304){
@@ -30,16 +70,21 @@ for(let i = 0,l = delBtns.length;i<l;i++){
             }            
         }
 }
-var editBtns = document.getElementsByClassName("btn-white")
-for(let i = 0,l = editBtns.length;i<l;i++){
-    let newsid = editBtns[i].getAttribute("flag")
-    editBtns[i].onclick = function editNews(){
+//批量删除
+var delAll = document.getElementById("delectall")
+delAll.onclick = function delAllNews(){
+    let checkedBox = document.getElementsByClassName("checkbox")
+    var a = []
+    for(let i = 0,l = checkedBox.length;i<l;i++){
+        if(checkedBox[i].checked){
+            a.push(checkedBox[i].getAttribute("flag"))
+        }
+    }
     let xhr =new XMLHttpRequest()
-    //post方法如下
-                        //删除操作的地址↓↓↓↓
-       xhr.open("post", "http://localhost:8080?", true)
+    //                ↓↓删除地址
+    xhr.open("post", "http://localhost:8080?", true)
        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-       xhr.send(newsid)
+       xhr.send(a)
        xhr.onreadystatechange = function(){
             if(xhr.readyState === 4){
                 if(xhr.status>=200 && xhr.status<300 || xhr.status===304){
@@ -56,10 +101,10 @@ for(let i = 0,l = editBtns.length;i<l;i++){
 //    xhr.open("post", "http://localhost:8080?", true)
 //    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
 //    xhr.send(join(data))
-// //get方法如下
-//                //删除操作的地址↓↓↓↓
-//     // xhr.open("get","http://localhost:8080?"+join(data),true)
-//     // xhr.send()
+// get方法如下
+//                删除操作的地址↓↓↓↓
+//     xhr.open("get","http://localhost:8080?"+join(data),true)
+//     xhr.send()
 //     xhr.onreadystatechange = function(){
 //         if(xhr.readyState === 4){
 //             if(xhr.status>=200 && xhr.status<300 || xhr.status===304){
