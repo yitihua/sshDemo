625d9e239c1f68de498d3ca9faaedce281383893
BUZA2018 <44996632+BUZA2018@users.noreply.github.com>
2019-05-22 13:27:37 +0800

    Add files via upload

--- sshDemo/src/main/webapp/static/js/indexStart.js
+++ sshDemo/src/main/webapp/static/js/indexStart.js
@@ -1,4 +1,17 @@
-var ctx = pageContext.request.contextPath
+function GetUrlRelativePath()
+{
+    var url = document.location.toString();
+    var arrUrl = url.split("//");
+
+    var start = arrUrl[1].indexOf("/");
+    var relUrl = arrUrl[1].substring(start);//stop省略，截取从start开始到结尾的所有字符
+
+    if(relUrl.indexOf("?") != -1){
+        relUrl = relUrl.split("?")[0];
+    }
+    return relUrl;
+}
+var ctx = GetUrlRelativePath();
 //用一个json形式的data变量储存你要传输的数据
 var data ={
     name: "buza",
